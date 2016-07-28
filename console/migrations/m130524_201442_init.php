7e8dad95f83e24952f959e03af69addde6d27f97
thanhlbk <thanhlbk@rikkeisoft.com>
2016-07-28 14:28:07 +0700

    create user db

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -19,7 +19,10 @@ class m130524_201442_init extends Migration
             'password_hash' => $this->string()->notNull(),
             'password_reset_token' => $this->string()->unique(),
             'email' => $this->string()->notNull()->unique(),
-
+            'role' => $this->smallInteger()->notNull()->defaultValue(2),
+            'device_type' => $this->smallInteger()->notNull()->defaultValue(0),
+            'device_token' => $this->string(),
+            
             'status' => $this->smallInteger()->notNull()->defaultValue(10),
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
