31cb95d90e8d918531c3ebe1eda2af5cf052b4b4
thanhlbk <thanhlbk@rikkeisoft.com>
2016-08-18 10:34:32 +0700

    fix permission folder

--- run.sh
+++ run.sh
@@ -3,9 +3,12 @@ chmod 777 -R common/models/
 chmod 777 -R backend/controllers/
 chmod 777 -R backend/views/
 chmod 777 -R backend/models/
+chmod 777 -R backend/runtime
 
 chmod 777 -R frontend/controllers/
 chmod 777 -R frontend/views/
 chmod 777 -R frontend/models/
+chmod 777 -R frontend/runtime
 
 chmod 777 -R api/web/uploads/
+chmod 777 -R api/runtime
