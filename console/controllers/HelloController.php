edc5651a46918da1489ccb9c622d269e9085776b
thanhle <thanhle09t2bkdn@gmail.com>
2016-09-04 10:42:03 +0700

    firebase

--- console/controllers/HelloController.php
+++ console/controllers/HelloController.php
@@ -26,7 +26,7 @@ class HelloController extends Controller
      */
     public function actionIndex($message = 'hello world')
     {
-        echo $message . "\n";
+        echo $message . "\n";        
         
     }
     public function actionSend($email){
