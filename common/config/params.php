bb093af11e5fb56f170a915d1dc072468626d3e6
thanhle <thanhle09t2bkdn@gmail.com>
2016-09-04 14:25:23 +0700

    set jwt time

--- common/config/params.php
+++ common/config/params.php
@@ -3,6 +3,7 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
+    'jwtExpire' => 86400*14,
     'fcm' => [
     'apiKey' => 'abc'
     ]
