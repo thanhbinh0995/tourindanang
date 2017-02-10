36191be73116718387cde9dc9f908e43137463a3
dinhduy7895 <dinhvanduy7895@gmail.com>
2017-02-10 19:25:48 +0700

    Tour Controllers

--- frontend/controllers/TourController.php
+++ frontend/controllers/TourController.php
@@ -7,9 +7,11 @@ class TourController extends \yii\web\Controller
 {
     public function actionIndex()
     {
-        return $this->render('index');
+        $tours = Tour::find()->limit(4)->orderBy('id DESC')->all(); 
+        return $this->render('index', [
+              'tours' => $tours
+       
+        ]);
     }
-
-
 
 }
