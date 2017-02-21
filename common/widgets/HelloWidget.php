0095eb7e27eb49e4cd29e7825c38004a6147c305
dinhduy7895 <dinhvanduy7895@gmail.com>
2017-02-21 16:08:49 +0700

    widget main

--- common/widgets/HelloWidget.php
+++ common/widgets/HelloWidget.php
@@ -8,33 +8,41 @@ use yii\web\View;
 use yii\web\JqueryAsset;
 class HelloWidget extends Widget 
 {   
-    public $typesTest;
+    public $typesName;
+    public $addressesName;
+    public $days;
     public $message;
+    public $content;
     public function init()
     {
         parent::init();
-        $this->typesTest = (new \yii\db\Query())
+        $this->typesName = (new \yii\db\Query())
                 ->select('id,name')
                 ->from('type')
                 ->all();
         // var_dump($this->typesTest);
         // exit();
-        // $addressesName = (new \yii\db\Query())
-        //         ->select('id,name')
-        //         ->from('address')
-        //         ->all();     
-        // $days = (new \yii\db\Query())
-        //         ->select('dayTour')
-        //         ->from('tour')
-        //         ->all(); 
-        $this->message = "Hello Yiifdfa2";
+        $this->addressesName = (new \yii\db\Query())
+                ->select('id,name')
+                ->from('address')
+                ->all();     
+        $this->days = (new \yii\db\Query())
+                ->select('dayTour')
+                ->from('tour')
+                ->all(); 
+       // $this->message = "Hello Yiifdfa2";
     }
 
     public function run() {
         parent::run();
         // HelloWidget::register($this->getView());
         // return $this->render('main', ['message' => $this->message]);
-        return Html::encode($this->render('main', ['typesTest' => $this->typesTest]));
+        return $this->render('main', [
+            'typesName' => $this->typesName,
+            'addressesName' => $this->addressesName,
+            'days' => $this->days,
+            'content' => $this->content,
+            ]);
         // return $this->render('@frontend/views/site/index', ['typesTest' => $this->typesTest]);
     }
 }
